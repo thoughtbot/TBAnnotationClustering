19ba8ca47fcc6a92015fd98be0bdb0af242b8d58
Theodore Calmes <theo@thoughtbot.com>
2013-09-27 14:11:37 -0700

    Initial Commit

---
+++ TBAnnotationClustering/TBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TBAppDelegate.h
+//  TBAnnotationClustering
+//
+//  Created by Theodore Calmes on 9/27/13.
+//  Copyright (c) 2013 Theodore Calmes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TBAnnotationClustering/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TBAnnotationClustering
+//
+//  Created by Theodore Calmes on 9/27/13.
+//  Copyright (c) 2013 Theodore Calmes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TBAppDelegate class]));
+    }
+}
